d6644339f181882795c36a2e05adfbbe7e513a2e
Mehreen M <90943797+mehreenmirza@users.noreply.github.com>
2022-06-12 18:41:58 -0400

    Update Education.js

--- Education.js
+++ Education.js
@@ -8,7 +8,7 @@ function Education() {
         <div className='university'>
             <h1> University of Virginia (UVA) </h1>
             <h5> Bachelor's Degree, Computer Science - December 2022 </h5> 
-            <h6> University Judiciary Committee, Charlottesville Alliance for Refugees (President) </h6> 
+            <h6> University Judiciary Committee, Charlottesville Alliance for Refugees HI (President) </h6> 
         </div>
     </div>
   )

de0fffed9ca808c1cac4c7ae1bf5a186309c9792
mehreenmirza <mmm4hna@virginia.edu>
2022-06-12 18:33:11 -0400

    M

--- Education.js
+++ Education.js
@@ -6,7 +6,7 @@ function Education() {
     <div className='education'>
         <h1> EDUCATION </h1>
         <div className='university'>
-            <h1> University of Virginia </h1>
+            <h1> University of Virginia UVA </h1>
             <h5> Bachelor's Degree, Computer Science - December 2022 </h5> 
             <h6> University Judiciary Committee, Charlottesville Alliance for Refugees (President) </h6> 
         </div>
